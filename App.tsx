32d8bde40fa090261a71007556d6ec58ea518f3e
Ramos159 <edwinramos269@gmail.com>
2020-01-08 13:59:41 -0500

    basic state changing with buttons and inputs

--- App.tsx
+++ App.tsx
@@ -1,19 +1,55 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { Component } from 'react';
+import { Text, View, TextInput, Button} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-    </View>
-  );
-}
+export default class Playground extends Component {
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  state = {
+    input:"",
+    final:""
+  }
+
+  configureInput = () => {
+    if(this.state.input){
+      return `you typed: ${this.state.input}` 
+    }
+    if(!this.state.input){
+      return "you haven't typed anything!"
+    }
+  }
+
+  handleTextInput = (event) => {
+    console.log(event)
+    this.setState({
+      ...this.state,
+      input:event.nativeEvent.text
+    })
+  }
+
+  submittedWord = () => {
+    return this.state.final ? <Text style={{fontSize:30}}>you submitted: {this.state.final}</Text> : null
+  }
+
+  handleSubmit = (event) => {
+    this.setState({
+      input:'',
+      final:event.nativeEvent.text
+    })
+  }
+
+  render() {
+    return (
+      <View style={{ flex: 1, justifyContent: "center", alignItems: "center"}}>
+        {this.submittedWord()}
+        <Text style={{fontSize:25,height:80}}>{this.configureInput()}</Text>
+        <TextInput 
+            placeholder="type something here!" 
+            onChange={(event)=>this.handleTextInput(event)} 
+            value={this.state.input} 
+            onSubmitEditing={(event)=>this.handleSubmit(event)}/>
+        <Button
+        onPress={() => {alert('You tapped the button!')}}
+        title="Press Me"/>
+      </View>
+    );
+  }
+};
