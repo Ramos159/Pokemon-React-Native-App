1a094999378543f303eb07c319e7ce287738c8b0
Ramos159 <edwinramos269@gmail.com>
2020-02-11 19:23:17 -0500

    okay looking form for authscreen

--- src/Profile/authFormScreen.tsx
+++ src/Profile/authFormScreen.tsx
@@ -1,6 +1,6 @@
 import React,{ Component } from 'react'
-import { View,TextInput } from 'react-native'
-import { Input,Icon } from 'react-native-elements'
+import { View,Text } from 'react-native'
+import { Input,Icon,Button } from 'react-native-elements'
 import StatusBarHeader from '../Components/statusBarHeader'
 import NavigationPropType from '../customTypes/navigationPropType'
 
@@ -26,19 +26,22 @@ export default class AuthFormScreen extends Component<Props,State>{
         login: true
     }
 
-    handleTextChange = (event): void => {
-        console.log(event)
+    handleTextChange = (event,stringParam): void => {
+        // this is the literally text from the event
+        const text = event.nativeEvent.text
+
+        console.log(text)
+        console.log(stringParam)
     }
 
     loginOrRegister = (): JSX.Element => {
-        // components for login and register
-        // TextInput has no name prop, so i'll pass a string arguement to the event handler
+        // login form as a function called login
         const login = () => {
             return(
                 <>
                 <Input
                     placeholder='Username'
-                    onChange={(event)=>this.handleTextChange(event)}
+                    onChange={(event)=>this.handleTextChange(event,"username")}
                     leftIcon={
                         <Icon
                             name='user'
@@ -49,7 +52,7 @@ export default class AuthFormScreen extends Component<Props,State>{
                 <Input
                     placeholder='Password'
                     secureTextEntry={true}
-                    onChange={(event)=>this.handleTextChange(event)}
+                    onChange={(event)=>this.handleTextChange(event,"password")}
                     leftIcon={
                         <Icon
                             name='lock'
@@ -60,17 +63,75 @@ export default class AuthFormScreen extends Component<Props,State>{
             </>
             )
         }
-        const register = <View style={{flex:1,backgroundColor:"white", alignContent:'center'}}><TextInput placeholder="Username" onChange={(event)=>this.handleTextChange(event)}/><TextInput placeholder="Password" onChange={(event)=>{this.handleTextChange(event)}}/><TextInput placeholder="Password Confirmation" onChange={(event)=>{this.handleTextChange(event)}}/><TextInput placeholder="Email" onChange={(event)=>this.handleTextChange(event)}/></View>
-        return this.state.login ? login() : register
+        // same as above but for register form
+        const register = () =>{
+            return(
+                <>
+                <Input
+                    placeholder='Username'
+                    onChange={(event)=>this.handleTextChange(event,"username")}
+                    leftIcon={
+                        <Icon
+                            name='user'
+                            type='feather'
+                        />
+                    }
+                    />
+                <Input
+                    placeholder='Password'
+                    secureTextEntry={true}
+                    onChange={(event)=>this.handleTextChange(event,"password")}
+                    leftIcon={
+                        <Icon
+                            name='lock'
+                            type='feather'
+                        />
+                    }
+                />
+                <Input
+                    placeholder='Email'
+                    onChange={(event)=>this.handleTextChange(event,"email")}
+                    leftIcon={
+                        <Icon
+                            name='mail'
+                            type='feather'
+                        />
+                    }
+                />
+            </>
+            )
+        }
+        return this.state.login ? login() : register()
     }
 
+    handleButtonTitle = ():string => {
+        return this.state.login ? "Log in" : "Register"
+    }
+
+    handleFormChange = ():void => {
+        // change a bit of state to trigger rerender and show other form
+        this.setState((prevState)=>{
+            return{
+                login: !prevState.login
+            }
+        })
+    }
+
+    handleChangeFormText = (): string => {
+        // change text depending on form thats present
+        return this.state.login ? "New User? Register here!" : "Have an Account? Log in here!"
+    }
 
     render(){
         return(
             <>
                 <StatusBarHeader/>
-                <View style={{flex:1,backgroundColor:"white", alignContent:'center'}}>
-                {this.loginOrRegister()}
+                <View style={{flex:1,backgroundColor:"white", alignItems:'center',justifyContent:'center'}}>
+                    <View style={{flex:1,backgroundColor:"white", alignItems:'center',justifyContent:'center',width:300}}>
+                        {this.loginOrRegister()}
+                        <Button title={this.handleButtonTitle()}/>
+                        <Text onPress={()=>{this.handleFormChange()}}>{this.handleChangeFormText()}</Text>
+                    </View>
                 </View>
             </>
         )
